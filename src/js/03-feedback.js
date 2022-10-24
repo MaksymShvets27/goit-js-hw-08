c485a034f28a49b8a958395cb3e83bb245f603cf
Maksym Shvets <maxim27061993@outlook.com>
2022-10-24 18:47:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,7 @@
 const formRef = document.querySelector(".feedback-form");
 const inputRef = document.querySelector("input");
 const messRef = document.querySelector("textarea");
-window.addEventListener("load", () => {
-    const startData = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (!inputRef.value) {
-        inputRef.value = "";
-    } else {
-        inputRef.value = `${startData.email}`;
-    };
-    if (!messRef.value) {
-        messRef.value = "";
-    } else {
-        messRef.value = `${startData.message}`;
-    }
-});
+
 
 
 formRef.addEventListener("submit", (event) => {
@@ -28,5 +16,10 @@ formRef.addEventListener("submit", (event) => {
     localStorage.setItem("feedback-form-state", JSON.stringify(data));
     formRef.reset();
     console.log(localStorage.getItem("feedback-form-state"));
+    window.addEventListener("load", () => {
+        const startData = JSON.parse(localStorage.getItem("feedback-form-state"));
+        inputRef.value = `${startData.email}`;
+        messRef.value = `${startData.message}`;
+    })
 }
 );
